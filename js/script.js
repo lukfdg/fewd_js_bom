d7a5b0cd95d140b61a28ed90e0a05d7d604378c4
Lucas_Souza <lukfdg@gmail.com>
2020-03-03 15:33:02 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var msg = '<h2>browser window</h2><p>width: ' + window.innerWidth + '</p>';
-msg += '<p>height: ' + window.innerHeight + '</p>';
+var msg = '<h2>Opening Times</h2><p>Mon-Fri: ' + window.innerWidth + '</p>';
+msg += '<p>Sat-Sun: ' + window.innerHeight + '</p>';
 msg += '<h2>history</h2><p>items: ' + window.history.length + '</p>';
 msg += '<h2>screen</h2><p>width: ' + window.screen.width + '</p>';
 msg += '<p>height: ' + window.screen.height + '</p>';
@@ -17,10 +17,10 @@ var el2 = document.getElementById('footer');
 el2.innerHTML = msg2;
 
 // Generate random number between 1 and 10
-var randomNum = Math.floor((Math.random() * 10) + 1);
+var randomNum = Math.floor((Math.random() * 100) + 1);
 
 var el3 = document.getElementById('info');
-el3.innerHTML += '<h2>random number</h2><p>' + randomNum + '</p>';
+el3.innerHTML += '<h2>Tables Avaliable</h2><p>' + randomNum + '</p>';
 
 // Create a variable to hold a new Date object (defaults to now)
 var today = new Date();
@@ -46,4 +46,4 @@ difference = (difference / 31556900000);
 // Create a variable called elMsg to hold the element whose id attribute has a value of message
 var elMsg = document.getElementById('message');
 // Write the message into that element.
-elMsg.textContent = Math.floor(difference) + ' years of online travel advice';
+elMsg.textContent = Math.floor(difference) + ' years putting art on the plate';
